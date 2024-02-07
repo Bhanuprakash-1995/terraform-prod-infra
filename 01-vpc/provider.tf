d4e381c8eb80a8c0c12bea43a6f2062021943af8
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-02-07 17:51:51 +0530

    update

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "roboshop-01"
-    key            = "terraforms-vpc"
+    key            = "terraforms-vpc-prod"
     region         = "us-east-1"
     dynamodb_table = "roboshopapp-lock"
   }
